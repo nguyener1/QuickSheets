74992ef99e15d91dc11c486c56585b0239133617
nguyener1 <nguyentom16@gmail.com>
2020-03-11 00:28:09 -0400

    signature can be made on mobile devices

--- resources/js/signature-module.js
+++ resources/js/signature-module.js
@@ -17,13 +17,19 @@ class SignTool {
   }
 
   initEvents() {
-    $('#canvas').on("mousemove", (e) => this.onMouseMove(e))
-    $('#canvas').on("mousedown", (e) => this.onMouseDown(e))
-    $('#canvas').on("mouseup", () => this.onMouseUp())
-    $('#canvas').on("mouseout", () => this.onMouseOut())
-    $('#canvas').on("mouseenter", (e) => this.onMouseEnter(e))
+    this.canvas.addEventListener("mousemove", (e) => this.onMouseMove(e), false)
+    this.canvas.addEventListener("mousedown", (e) => this.onMouseDown(e), false)
+    this.canvas.addEventListener("mouseup", () => this.onMouseUp(), false)
+    this.canvas.addEventListener("mouseout", () => this.onMouseOut(), false)
+    this.canvas.addEventListener("mouseenter", (e) => this.onMouseEnter(e))
+    this.canvas.addEventListener("touchstart", (e) => this.onTouchStart(e), false)
+    this.canvas.addEventListener("touchend", () => this.onTouchEnd(), false)
+    this.canvas.addEventListener("touchmove", (e) => this.onTouchMove(e), false)
+    document.body.addEventListener("touchstart", (e) => this.stopScrolling(e), { passive: false })
+    document.body.addEventListener("touchend", (e) => this.stopScrolling(e), { passive: false })
+    document.body.addEventListener("touchmove", (e) => this.stopScrolling(e), { passive: false })
   }
-
+//Signature by Mouse
   onMouseDown(e) {
     this.isMouseClicked = true
     this.updateCurrentPosition(e)
@@ -56,7 +62,45 @@ class SignTool {
       this.currX = e.clientX - this.canvas.getBoundingClientRect().left
       this.currY = e.clientY - this.canvas.getBoundingClientRect().top
   }
+    
+    onTouchStart(e){
+        var touch = e.touches[0]
+        this.isMouseInCanvas = true
+        var mouseEvent = new MouseEvent("mousedown", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        })
+        this.canvas.dispatchEvent(mouseEvent)
+    }
+    
+    onTouchEnd(){
+        this.isMouseInCanvas = false
+        this.onMouseUp()
+    }
 
+    onTouchMove(e){
+        var touch = e.touches[0]
+        var mouseEvent = new MouseEvent("mousemove", {
+            clientX: touch.clientX,
+            clientY: touch.clientY
+        })
+        this.canvas.dispatchEvent(mouseEvent)
+    }
+
+//    updateCurrentTouchPos(e) {
+//        this.prevX = this.currX
+//        this.prevY = this.currY
+//        this.currX = e.touches[0].clientX - this.canvas.getBoundingClientRect().left
+//        this.currY = e.touches[0].clientY - this.canvas.getBoundingClientRect().top
+//    }
+    
+    //prevent scrolling
+    stopScrolling(e){
+        if (e.target == this.canvas){
+            e.preventDefault()
+        }
+    }
+    
   draw() {
     this.ctx.beginPath()
     this.ctx.moveTo(this.prevX, this.prevY)
@@ -66,6 +110,12 @@ class SignTool {
     this.ctx.stroke()
     this.ctx.closePath()
   }
+    
+
 }
+
+//Signature by Touch
+ 
+
 
 var canvas = new SignTool()
